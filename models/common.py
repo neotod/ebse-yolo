c5b1ded34d52a70fa68cee2c8b929d1ddc208a8b
neotod <neotod@protonmail.com>
2024-07-03 14:11:26 +0330

    adding eca-net to yolo-spdconv

--- models/common.py
+++ models/common.py
@@ -53,6 +53,30 @@ def autopad(k, p=None, d=1):  # kernel, padding, dilation
         p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
     return p
 
+class eca_layer(nn.Module):
+    """Constructs a ECA module.
+
+    Args:
+        k_size: Adaptive selection of kernel size
+    """
+    def __init__(self, k_size=3):
+        super(eca_layer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.conv = nn.Conv1d(1, 1, kernel_size=k_size, padding=(k_size - 1) // 2, bias=False) 
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        # feature descriptor on the global spatial information
+        y = self.avg_pool(x)
+
+        # Two different branches of ECA module
+        y = self.conv(y.squeeze(-1).transpose(-1, -2)).transpose(-1, -2).unsqueeze(-1)
+
+        # Multi-scale information fusion
+        y = self.sigmoid(y)
+
+        return x * y.expand_as(x)
+
 class space_to_depth(nn.Module):
     # Changing the dimension of the Tensor
     # scale = 2
