3770a31506ed1eeb82bac7cac92c9c772c913eaa
neotod <neotod@protonmail.com>
2024-07-11 13:41:56 +0330

    tiny change

--- code/main.py
+++ code/main.py
@@ -56,6 +56,7 @@ with open(f'{cur_dir}/dota_1_5.yaml', 'w') as file:
 
 print("YAML file 'dota_1_5.yaml' has been created successfully.")
 
+
 # train the model
 print('training the model')
 os.system(f"python3 train.py --data dota_1_5.yaml --workers 4 --cfg ./models/yolov5s__spd_conv__eca_net__bifpn.yaml --batch-size 64 --epochs 3 --sync-bn --project yolov5s__spd_conv__eca_net --name yolov5s__spd_conv__eca_net --hyp hyp.scratch_s.yaml")
